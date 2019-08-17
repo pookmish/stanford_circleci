a2f4e994c9764f0d3790f38d38d6560d38cb04c2
Mike Decker <pookmish@gmail.com>
2019-08-16 20:03:41 -0700

    phpunit command

--- src/Commands/RoboFile.php
+++ src/Commands/RoboFile.php
@@ -7,11 +7,39 @@ use Robo\Tasks;
 class RoboFile extends Tasks {
 
   /**
-   * @command sws-hello
+   * @command phpunit
    */
-  public function hello() {
-    $this->yell('HELLO');
-  }
+  public function phpunit($root_path) {
+    if (is_dir_empty($root_path)) {
+      $this->taskComposerCreateProject()
+        ->arg('drupal-composer/drupal-project:8.x-dev')
+        ->option($root_path)
+        ->option('no-interaction')
+        ->run();
 
+      $this->taskComposerRequire()
+        ->option('wikimedia/composer-merge-plugin')
+        ->run();
+
+      $this->taskComposerConfig()
+        ->option('xtra.merge-plugin.require', '../test/composer.json')
+        ->run();
+    }
+    else {
+      $this->taskComposerUpdate()->run();
+    }
+
+    $this->_symlink("$root_path/docroot", "$root_path/web");
+    $this->_mkdir("$root_path/docroot/modules/custom");
+    $this->_copy(__DIR__ . '/config/phpunit.xml', "$root_path/docroot/core/phpunit.xml", TRUE);
+
+    $this->_copy("$root_path/../test", "$root_path/docroot/modules/custom");
+
+    $this->taskExec('../vendor/bin/phpunit')
+      ->dir("$root_path/docroot")
+      ->option('c', 'core')
+      ->arg('modules/custom')
+      ->run();
+  }
 
 }
