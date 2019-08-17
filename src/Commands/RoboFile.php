fd0b5b68c1a914500ac736cab58868ff8378be4b
Mike Decker <pookmish@gmail.com>
2019-08-16 20:40:10 -0700

    universal extensions

--- src/Commands/RoboFile.php
+++ src/Commands/RoboFile.php
@@ -29,17 +29,20 @@ class RoboFile extends Tasks {
       $this->taskComposerUpdate()->run();
     }
 
-    $this->_symlink("$root_path/web", "$root_path/docroot");
-    $this->_deleteDir("$root_path/docroot/modules/custom");
-    $this->_mkdir("$root_path/docroot/modules/custom");
-    $this->_copy(dirname(dirname(dirname(__FILE__))) . '/config/phpunit.xml', "$root_path/docroot/core/phpunit.xml", TRUE);
+    $extension_type = $this->getExtensionType("$root_path/../test");
+    $name = $this->getExtensionName("$root_path/../test");
 
-    $this->_symlink("$root_path/../test", "$root_path/docroot/modules/custom");
+    $this->_symlink("$root_path/web", "$root_path/docroot");
+    $this->_deleteDir("$root_path/docroot/{$extension_type}s/custom");
+    $this->_mkdir("$root_path/docroot/{$extension_type}s/custom");
+
+    $this->_copy(dirname(dirname(dirname(__FILE__))) . '/config/phpunit.xml', "$root_path/docroot/core/phpunit.xml", TRUE);
+    $this->_symlink("$root_path/../test", "$root_path/docroot/{$extension_type}s/custom/$name");
 
     $this->taskExec('../vendor/bin/phpunit')
       ->dir("$root_path/docroot")
       ->option('config', 'core', '=')
-      ->arg('modules/custom')
+      ->arg("{$extension_type}s/custom/$name")
       ->run();
   }
 
@@ -48,6 +51,19 @@ class RoboFile extends Tasks {
       return NULL;
     }
     return (count(scandir($dir)) == 2);
+  }
+
+  protected function getExtensionName($dir) {
+    $files = glob("$dir/*.info.yml");
+    $info_file = basename($files[0]);
+    return str_replace('.info.yml', '', $info_file);
+  }
+
+  protected function getExtensionType($dir) {
+    $name = $this->getExtensionName($dir);
+    $info_contents = file_get_contents("$dir/$name.info.yml");
+    $matches = preg_grep('/^type:.*?$/x', explode("\n", $info_contents));
+    return trim(str_replace('type: ', '', reset($matches)));
   }
 
 }
